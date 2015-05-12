347434d2575234c81793cb15cbb4654ea8f44818
Gei Lin <gmonaie@gmail.com>
2015-05-12 04:37:10 +0000

    fix utility functions

--- R/eventUtils.R
+++ R/eventUtils.R
@@ -7,10 +7,22 @@ region <- function (x, ...)
 #' @export
 region.character <- function (x, Date, expired = TRUE, silent = FALSE, ...)
 {
-  xi <- getEvent(x, silent = TRUE)
-  if (is.event(xi)) {
+  # depdning on whether it's a session.name,
+  # market.name
+  # exchange.name
+  # or what not, decide how to deal with it
+  if (is.event.name(x)) {
+    xi <- getEvent(x, silent = TRUE)
     region.event(xi, Date = Date, expired = expired,
-                      silent = silent, ... = ...)
+                  silent = silent, ... = ...)
+  } else if (is.exchange.name(x)) {
+    xi <- getExchange(x, silent = TRUE)
+    region.exchange(xi, Date = Date, expired = expired,
+                  silent = silent, ... = ...)
+  } else if (is.market.name(x)) {
+     xi <- getMarket(x, silent = TRUE)
+    region.market(xi, Date = Date, expired = expired,
+                  silent = silent, ... = ...)
   }
   else {
     if (!isTRUE(silent)) {
@@ -22,7 +34,21 @@ region.character <- function (x, Date, expired = TRUE, silent = FALSE, ...)
 #' @export
 region.event <- function (x, Date, expired = TRUE, silent = FALSE, ...)
 {
-  if (is.session(x))
+  if (is.event(x))
+    return(region(x))
+  else {
+    xp <- x[["region"]]
+    if (length(xp) == 0)
+      return(NULL)
+    else
+      return(xp)
+  }
+}
+
+#' @export
+region.market <- function (x, Date, expired = TRUE, silent = FALSE, ...)
+{
+ if (is.market(x))
     return(region(exchange(x)))
   else {
     xp <- x[["region"]]
@@ -31,6 +57,19 @@ region.event <- function (x, Date, expired = TRUE, silent = FALSE, ...)
     else
       return(xp)
   }
+}
+
+#' @export
+region.exchange <- function (x, Date, expired = TRUE, silent = FALSE, ...)
+{
+  if (is.exchange(x)) {
+    xp <- x[["region"]]
+    if (length(xp) == 0)
+      return(NULL)
+    else
+      return(xp)
+  }
+  else NextMethod("region")
 }
 
 #' @export
@@ -48,6 +87,9 @@ exchange <- function (x, ...)
 #' @export
 exchange.character <- function (x, Date, expired = TRUE, silent = FALSE, ...)
 {
+  if (is.exchange.name(x)) {
+    return(x)
+  }
   xi <- getMarket(x, silent = TRUE)
   if (is.session(xi)) {
     exchange.session(xi, Date = Date, expired = expired,
@@ -65,6 +107,32 @@ exchange.session <- function (x, Date, expired = TRUE, silent = FALSE, ...)
 {
   if (is.session(x)) {
     xp <- x[["exchange"]]
+    if (length(xp) == 0)
+      return(NULL)
+    else
+      return(xp)
+  }
+  else NextMethod("exchange")
+}
+
+#' @export
+exchange.market <- function (x, Date, expired = TRUE, silent = FALSE, ...)
+{
+  if (is.market(x)) {
+    xp <- x[["exchange"]]
+    if (length(xp) == 0)
+      return(NULL)
+    else
+      return(xp)
+  }
+  else NextMethod("exchange")
+}
+
+#' @export
+exchange.exchange <- function (x, Date, expired = TRUE, silent = FALSE, ...)
+{
+  if (is.exchange(x)) {
+    xp <- x[["primary_id"]]
     if (length(xp) == 0)
       return(NULL)
     else
